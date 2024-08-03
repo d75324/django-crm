c4ca6214688a53a791572678f50b734a621f0958
LVO <112906515+d75324@users.noreply.github.com>
2024-08-03 18:01:23 -0300

    revisar modelos

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from website.models import Record
 
-admin.site.register(Record)
+class RecordsBackEnd(admin.ModelAdmin):
+    list_display = (
+        'created_at',
+        'first_name',
+        'last_name',
+        'email',
+        'city',
+        )
+
+admin.site.register(Record, RecordsBackEnd)
